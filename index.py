af36a12399fec0824e075344a5f154f40c9c4607
Yash Sehgal <96070479+yash022@users.noreply.github.com>
2022-11-18 14:41:04 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,19 @@
+from flask import Flask , redirect
+from pyairtable import Table
+    
+app = Flask(__name__)
+
+@app.route('/<id>')
+def index(id):
+    ids = []
+    urls = []
+    table = Table('keynabsTSGt63GDEw', 'appobl90u3RKofhha', 'urls')
+    for j in table.all():
+        ids.append((j['fields']['ID']).strip())
+    for k in table.all():
+        urls.append((k['fields']['URL']).strip())
+    if id in ids:
+        return redirect(("http://"+urls[ids.index(id)]), code=302)
+app.run()
+
+
